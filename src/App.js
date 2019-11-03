df399853ded974c9911b6b7c81fb1a47cb4efd5f
Oliver <ocben1@gmail.com>
2019-11-03 14:14:34 +1100

    fetch data with react hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const result = await fetch("https://orangevalleycaa.org/api/videos")
+      .then(response => response.json()
+      )
+      setData(result);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React PWA
-        </a>
+      <header>
+        <h1>Videos</h1>
       </header>
+      {data.map(video => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url} />
+        </div>
+      ))}
     </div>
   );
 }
