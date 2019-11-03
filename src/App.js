6efc070387ab8d40adeb78cfcc842dfbbc4b6e65
Oliver <ocben1@gmail.com>
2019-11-03 15:08:59 +1100

    going offline

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
+import video from "./videos/sculpture.mp4";
 
 function App() {
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    const fetchData = async() => {
-      const result = await fetch("https://orangevalleycaa.org/api/videos")
-      .then(response => response.json()
-      )
-      setData(result);
-    };
-    fetchData();
-  }, []);
   return (
     <div className="App">
       <header>
-        <h1> Art Videos</h1>
+        <h1>Art Videos</h1>
       </header>
-      {data.map(video => (
-        <div key={video.id}>
-          <h2>{video.name}</h2>
-          <video height={200} controls src={video.video_url} />
+        <div>
+          <video src={video} controls height={200} />
         </div>
-      ))}
     </div>
   );
 }
